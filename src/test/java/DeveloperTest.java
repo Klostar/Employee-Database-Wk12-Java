00cf87171c135fb821ee3e30c0ad224f2c22cd80
chloe alcorn <ca.alcorn@outlook.com>
2018-10-15 17:36:11 +0100

    dev has name, salary and ni number

--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,29 @@
+import Staff.Developer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeveloperTest {
+
+    Developer developer;
+
+    @Before
+    public void before(){
+        developer = new Developer("Chloe Alcorn", 22000.0, "JX115642C");
+    }
+
+    @Test
+    public void devHasSalary(){
+        assertEquals(22000.0, developer.getSalary(),1);
+    }
+
+    @Test
+    public void devHasName(){
+        assertEquals("Chloe Alcorn", developer.getName());
+    }
+    @Test
+    public void devHasNiNumber(){
+        assertEquals("JX115642C", developer.getNiNumber());
+    }
 }
